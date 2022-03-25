7f3169323a1842a49c25ecd0b59bcc2fded4a08f
Tannu <Tannu@DESKTOP-8ROBE7V>
2022-03-25 21:04:35 +0530

    first commit!

---
+++ javaproject/src/Unit_01/keyword.java
@@ -0,0 +1,43 @@
+package Unit_01;
+
+public class keyword {
+	//private:"Inside class only"
+	//Protected: Class+ same package
+	//public: Class+within the package+outside the package
+	//Default: No Keyword,package-private{Not outside package}
+	public class P2_Task02_ConstantsInJava {
+		//final keyword is used for assigning a constant value which will remain same throughout the program
+		public static final double Pi=3.14;
+
+		public static void main(String[] args) {
+			System.out.println(Pi);
+			main("Callingtheprogram");
+			
+			//syntax for creating new object
+			//class_name variable/object name=new class_name()
+			NewClass obj=new NewClass();
+			obj.add(1,2);
+			System.out.println(obj.Pi);
+
+		}
+		public static void main(String args)
+		{
+			System.out.println(Pi);
+		}
+	    void add(int a,int b)
+	    {
+	    	System.out.println(a+b);
+	    	System.out.println(Pi);
+	    }
+	}
+	class NewClass{
+		void add(int a,int b) {
+			//final double Pi=3.18 {constant value can't  be modified}
+		System.out.println(a+b);
+		//value of pi is imported from class P2_Task02_ConstantsInJava into the new class created
+		//another way of resolving the problem is by declaring the variable globally
+		//
+		System.out.println(P2_Task02_ConstantsInJava.Pi);
+		}
+	}
+}
